2cc960a6ef3d6662b19102e25a6da8642d4cf902
Maud Gilet <maud.gilet@gmail.com>
2018-11-26 17:45:26 +0100

    par precaution

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  get 'users/:id', to: 'users#show'
+
   resources :albums do
     resources :rentals, only: [:create, :show]
   end
